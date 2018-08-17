53195ee2114fea5fea4659bf8a0362e1e6011035
icezeros <icezeros@163.com>
2018-08-17 17:09:54 +0800

    wechatAuth

--- app/router.js
+++ app/router.js
@@ -6,5 +6,5 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-  router.get('/weixinAuth', controller.weixinAuth.index);
+  router.get('/wechatAuth', controller.weixinAuth.index);
 };
