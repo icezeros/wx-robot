f80d9dd108a50643c87f2444d784292ea9f513cf
icezeros <icezeros@163.com>
2018-08-17 17:27:36 +0800

    init

--- config/config.default.js
+++ config/config.default.js
@@ -1,11 +1,12 @@
 'use strict';
+require('dotenv').config();
 
 module.exports = appInfo => {
   const config = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1534493649375_4254';
-  config.wxToken = '';
+  config.wxToken = process.env.WXTOKEN;
 
   // add your config here
   config.middleware = [];
