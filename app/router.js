43d683c32afa03fef24e2a833dc02e08cf94490c
icezeros <icezeros@163.com>
2018-08-22 16:08:47 +0800

    test

--- app/router.js
+++ app/router.js
@@ -6,6 +6,6 @@
 module.exports = app => {
   const { router, controller } = app;
   // router.get('/', controller.home.index);
-  router.get('/wechatAuth', controller.wechatAuth.index);
-  router.all('/', controller.wechat.wechat);
+  // router.get('/wechatAuth', controller.wechatAuth.index);
+  router.all('/wechat', controller.wechat.wechat);
 };
