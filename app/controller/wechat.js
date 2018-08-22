a1140699e466d935e01d0c9608a0b5b26e86b8a1
icezeros <icezeros@163.com>
2018-08-22 15:56:14 +0800

    test

--- app/controller/wechat.js
+++ app/controller/wechat.js
@@ -2,7 +2,7 @@
  * @Author: icezeros
  * @Date: 2018-08-17 16:15:20
  * @Last Modified by: icezeros
- * @Last Modified time: 2018-08-21 16:07:42
+ * @Last Modified time: 2018-08-21 16:09:19
  */
 'use strict';
 const crypto = require('crypto');
@@ -11,8 +11,7 @@ const wechat = require('co-wechat');
 
 class WeixinAuthController extends Controller {}
 WeixinAuthController.prototype.wechat = wechat(
-  // 'Secrypto2018'
-  this.app.config.wxToken
+  'Secrypto2018'
   // 当有前置中间件设置 ctx.wx_token 和 ctx.wx_cryptor 时，这里配置随意填写
   // token: 'Secrypto2018',
   // appid: 'wx5972cd3080b9aae9',
